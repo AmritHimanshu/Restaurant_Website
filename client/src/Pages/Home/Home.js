0128b08dec648b9824bcb322a1475824fcba7469
AmritHimanshu <amrit.ewp@gmail.com>
2023-09-12 15:31:20 +0530

    homepage user

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -28,11 +28,11 @@ function Home() {
   }, []);
 
   const navigate = useNavigate();
-  const user = useSelector(selectUser);
-  console.log(user)
+  // const user = useSelector(selectUser);
+  // console.log(user)
   
-  const [data, setData] = useState(user);
-  console.log(data)
+  // const [data, setData] = useState(user);
+  // console.log(data)
 
   const infoCardDetails = [
     {
@@ -128,7 +128,7 @@ function Home() {
         <div className="h-[100vh] px-10 xl:w-[52%] flex flex-col justify-center bg-[url(https://w0.peakpx.com/wallpaper/176/588/HD-wallpaper-indian-food-breakfast-healthy.jpg)] lg:bg-none bg-auto sm:bg-cover bg-no-repeat bg-top lg:bg-left">
           <div className="space-y-10 text-center">
             <div className="text-[30px] xl:text-[45px] text-center text-white lg:text-black font-bold">
-              {data?.name}'s Favourite Food Delivered Hot & Fresh 
+              Your Favourite Food Delivered Hot & Fresh 
             </div>
             <p className="text-white lg:text-gray-700 text-[14px] sm:text-[20px] 2xl:text-[30px] p-5 bg-black lg:bg-[#f6f6f6] bg-opacity-60 rounded-xl">
               Healthy switcher chefs do all the prep work, like peeding,
