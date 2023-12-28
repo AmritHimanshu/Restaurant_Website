7d762ff381c95377c3ed63f19322e247eff00ea6
AmritHimanshu <amrit.ewp@gmail.com>
2023-12-29 00:45:16 +0530

    updated authenticate function

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -5,14 +5,31 @@ const authenticate = async (req, res, next) => {
     try {
         const token = req.cookies.jwtoken;
         const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
+
+        // Check for token expiration
+        if (!verifyToken) {
+            throw new Error('Token has expired');
+        }
+
         const rootUser = await User.findOne({ _id: verifyToken._id, "tokens.token": token });
-        if (!rootUser) throw new Error('User not found');
+
+        if (!rootUser) {
+            throw new Error('User not found');
+        }
+
         req.token = token;
         req.rootUser = rootUser;
         req.userID = rootUser._id;
+
         next();
     } catch (error) {
-        res.status(401).send("Unauthorized: No token provided");
+        if (error.name === 'TokenExpiredError') {
+            res.status(401).send("Unauthorized: Token has expired");
+        } else {
+            res.status(401).send("Unauthorized: No valid token provided");
+        }
+
+        // Uncomment the line below for debugging purposes
         // console.log(error);
     }
 }
