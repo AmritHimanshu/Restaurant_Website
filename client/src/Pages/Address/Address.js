54c1b20c9f78c54fba898b18f0bf798068e7648d
AmritHimanshu <amrit.ewp@gmail.com>
2023-12-30 12:33:38 +0530

    added loading status

--- client/src/Pages/Address/Address.js
+++ client/src/Pages/Address/Address.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { selectUser } from '../../features/userSlice';
+import LoadingIcon from '../Loading/loading.png';
 
 function Address() {
   const [houseNumber, setHouseNumber] = useState('');
@@ -10,12 +9,13 @@ function Address() {
   const [landmark, setLandmark] = useState('');
   const [directions, setDirections] = useState('');
 
-  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
-  const user = useSelector(selectUser);
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
 
     const res = await fetch('https://demo-restaurant-omega.vercel.app/address', {
       method: 'POST',
@@ -41,6 +41,7 @@ function Address() {
       localStorage.setItem('address', JSON.stringify(address));
 
       navigate("/payment");
+      setLoading(false);
     }
   };
 
@@ -49,93 +50,99 @@ function Address() {
   },[])
 
   return (
-    <div className="my-24 w-[350px] md:w-[650px] m-auto p-5 xl:p-10 bg-white rounded-xl shadow-xl">
-      <h2 className="text-blue font-bold text-2xl pb-5">
-        Delivery Address
-      </h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-[15px] space-y-2">
-          <label htmlFor="houserNumber" className="text-lg text-gray-600">
-            House/Flat/Block No. <span className="text-[red]">*</span>
-          </label>
-          <input
-            type="text"
-            id="houserNumber"
-            name="houserNumber"
-            value={houseNumber}
-            onChange={(e) => setHouseNumber(e.target.value)}
-            required
-            className="outline-0 p-2 text-lg w-full border-2 border-gray-600 placeholder:text-sm"
-          />
-        </div>
-        <div className="mb-[15px] space-y-2">
-          <label htmlFor="apartment" className="text-lg text-gray-600">
-            Apartment/Road/Area <span className="text-[red]">*</span>
-          </label>
-          <input
-            type="text"
-            id="apartment"
-            name="apartment"
-            value={apartment}
-            onChange={(e) => setApartment(e.target.value)}
-            required
-            className="outline-0 p-2 text-lg w-full border-2 border-gray-600 placeholder:text-sm"
-          />
-        </div>
-        <div className="mb-[15px] space-y-2">
-          <label htmlFor="pincode" className="text-lg text-gray-600">
-            Pincode <span className="text-[red]">*</span>
-          </label>
-          <input
-            type="text"
-            id="pincode"
-            name="pincode"
-            value={pincode}
-            onChange={(e) => setPincode(e.target.value)}
-            
-            className="outline-0 p-2 text-lg w-full border-2 border-gray-600 placeholder:text-sm"
-          />
-        </div>
-        <div className="mb-[15px] space-y-2">
-          <label htmlFor="landmark" className="text-lg text-gray-600">Lankmark (optional)</label>
-          <input
-            type="text"
-            id="landmark"
-            name="landmark"
-            value={landmark}
-            onChange={(e) => setLandmark(e.target.value)}
-            className="outline-0 p-2 text-lg w-full border-2 border-gray-600 placeholder:text-sm"
-          />
-        </div>
-        <div className="mb-[15px] space-y-2">
-          <label htmlFor="directions" className="text-lg text-gray-600">
-            Directions to Reach (optional)
-          </label>
-          <textarea
-            id="directions"
-            name="directions"
-            value={directions}
-            onChange={(e) => setDirections(e.target.value)}
-            className="outline-0 w-full p-2 text-lg w-full border-2 border-gray-600 placeholder:text-sm resize-none rounded-none"
-          />
-        </div>
-        <div className='flex items-center justify-between'>
-          <div
-            className="mt-5 p-3 bg-blue text-center text-gray-200 font-bold rounded-md duration-200 hover:bg-white hover:text-blue cursor-pointer"
-            onClick={() => { navigate('/delivery-address'); }}
-          >
-            Cancel
-          </div>
+    <>
+      {loading && <div className="h-[100vh] flex items-center justify-center">
+        <img src={LoadingIcon} alt="" className="w-[50px] animate-bounce" />
+      </div>}
 
-          <button
-            type="submit"
-            className="mt-5 p-3 bg-blue text-center text-gray-200 font-bold rounded-md duration-200 hover:bg-white hover:text-blue cursor-pointer"
-          >
-            Deliver Here
-          </button>
-        </div>
-      </form>
-    </div>
+      {!loading && <div className="my-24 w-[350px] md:w-[650px] m-auto p-5 xl:p-10 bg-white rounded-xl shadow-xl">
+        <h2 className="text-blue font-bold text-2xl pb-5">
+          Delivery Address
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-[15px] space-y-2">
+            <label htmlFor="houserNumber" className="text-lg text-gray-600">
+              House/Flat/Block No. <span className="text-[red]">*</span>
+            </label>
+            <input
+              type="text"
+              id="houserNumber"
+              name="houserNumber"
+              value={houseNumber}
+              onChange={(e) => setHouseNumber(e.target.value)}
+              required
+              className="outline-0 p-2 text-lg w-full border-2 border-gray-600 placeholder:text-sm"
+            />
+          </div>
+          <div className="mb-[15px] space-y-2">
+            <label htmlFor="apartment" className="text-lg text-gray-600">
+              Apartment/Road/Area <span className="text-[red]">*</span>
+            </label>
+            <input
+              type="text"
+              id="apartment"
+              name="apartment"
+              value={apartment}
+              onChange={(e) => setApartment(e.target.value)}
+              required
+              className="outline-0 p-2 text-lg w-full border-2 border-gray-600 placeholder:text-sm"
+            />
+          </div>
+          <div className="mb-[15px] space-y-2">
+            <label htmlFor="pincode" className="text-lg text-gray-600">
+              Pincode <span className="text-[red]">*</span>
+            </label>
+            <input
+              type="text"
+              id="pincode"
+              name="pincode"
+              value={pincode}
+              onChange={(e) => setPincode(e.target.value)}
+
+              className="outline-0 p-2 text-lg w-full border-2 border-gray-600 placeholder:text-sm"
+            />
+          </div>
+          <div className="mb-[15px] space-y-2">
+            <label htmlFor="landmark" className="text-lg text-gray-600">Lankmark (optional)</label>
+            <input
+              type="text"
+              id="landmark"
+              name="landmark"
+              value={landmark}
+              onChange={(e) => setLandmark(e.target.value)}
+              className="outline-0 p-2 text-lg w-full border-2 border-gray-600 placeholder:text-sm"
+            />
+          </div>
+          <div className="mb-[15px] space-y-2">
+            <label htmlFor="directions" className="text-lg text-gray-600">
+              Directions to Reach (optional)
+            </label>
+            <textarea
+              id="directions"
+              name="directions"
+              value={directions}
+              onChange={(e) => setDirections(e.target.value)}
+              className="outline-0 w-full p-2 text-lg w-full border-2 border-gray-600 placeholder:text-sm resize-none rounded-none"
+            />
+          </div>
+          <div className='flex items-center justify-between'>
+            <div
+              className="mt-5 p-3 bg-blue text-center text-gray-200 font-bold rounded-md duration-200 hover:bg-white hover:text-blue cursor-pointer"
+              onClick={() => { navigate('/delivery-address'); }}
+            >
+              Cancel
+            </div>
+
+            <button
+              type="submit"
+              className="mt-5 p-3 bg-blue text-center text-gray-200 font-bold rounded-md duration-200 hover:bg-white hover:text-blue cursor-pointer"
+            >
+              Deliver Here
+            </button>
+          </div>
+        </form>
+      </div>}
+    </>
   );
 }
 
--- client/src/Pages/Menu/Menu.js
+++ client/src/Pages/Menu/Menu.js
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { selectUser } from "../../features/userSlice";
 import Header from "../Header/Header";
 import FoodCard from "./components/FoodCard";
+import LoadingIcon from "../Loading/loading.png";
 import "../Menu/Menu.css";
 
 const Menu = () => {
@@ -12,6 +13,8 @@ const Menu = () => {
 
     const [cartLength, setCartLength] = useState();
 
+    const [loading, setLoading] = useState(false);
+
     const dataLen = (data) => {
         setCartLength(data);
     }
@@ -19,6 +22,7 @@ const Menu = () => {
     const [menu, setMenu] = useState();
 
     const getMenu = async () => {
+        setLoading(true);
         try {
             const res = await fetch('https://demo-restaurant-omega.vercel.app/getMenu', {
                 method: 'GET',
@@ -29,6 +33,7 @@ const Menu = () => {
 
             const data = await res.json();
             setMenu(data);
+            setLoading(false);
             
         } catch (error) {
             // console.log(error);
@@ -70,36 +75,42 @@ const Menu = () => {
                     <div className="my-10 text-[30px] text-center font-bold">Our Menu</div>
                 </div>
 
-                <div className="topSection">
-                    <div className="mealSelect">
+                {loading && <div className="h-[50vh] flex items-center justify-center">
+                    <img src={LoadingIcon} alt="" className="w-[50px] animate-bounce" />
+                </div>}
 
-                        {menu?.map((data, index) => (
-                            <button key={index} value={data.category} className={`mealButton`} onClick={selectMealType} >{data.category}</button>
-                        ))}
+                {!loading && <div>
+                    <div className="topSection">
+                        <div className="mealSelect">
 
-                        {/* <button value="Soup" className="mealButton mealActive" onClick={selectMealType} >Soup</button> */}
+                            {menu?.map((data, index) => (
+                                <button key={index} value={data.category} className={`mealButton`} onClick={selectMealType} >{data.category}</button>
+                            ))}
+
+                            {/* <button value="Soup" className="mealButton mealActive" onClick={selectMealType} >Soup</button> */}
+                        </div>
                     </div>
-                </div>
 
-                <div className="mb-20 flex flex-col items-center space-y-5">
-                    {
-                        menu?.map((food, index) => {
-                            if (food.category === mealType) {
-                                return food.items?.map((item) => (
-                                    <FoodCard key={item._id} id={item._id} title={item.name} price={item.price} image={item.image} func={dataLen} />
-                                ))
-                            }
-                        })
-                    }
-                </div>
-
-                {user && cartLength > 0 && <div className="px-2 lg:px-5 py-2 md:py-2 bg-blue flex flex-row md:flex-row items-center justify-between w-[350px] md:w-[600px] xl:w-[600px] rounded-md fixed bottom-1 left-[6%] md:left-[14%] lg:left-[22%] xl:left-[30%] 2xl:left-[34%] shadow-xl"
-                >
-                    <div className="text-[16px] text-white text-center">
-                        {cartLength} items added
+                    <div className="mb-20 flex flex-col items-center space-y-5">
+                        {
+                            menu?.map((food, index) => {
+                                if (food.category === mealType) {
+                                    return food.items?.map((item) => (
+                                        <FoodCard key={item._id} id={item._id} title={item.name} price={item.price} image={item.image} func={dataLen} />
+                                    ))
+                                }
+                            })
+                        }
                     </div>
-                    <Link to={`/cart`}>
-                        <div className="text-[15px] xl:text-[18px] text-white p-2 cursor-pointer hover:scale-105 hover:font-bold duration-300">View Cart</div></Link>
+
+                    {user && cartLength > 0 && <div className="px-2 lg:px-5 py-2 md:py-2 bg-blue flex flex-row md:flex-row items-center justify-between w-[350px] md:w-[600px] xl:w-[600px] rounded-md fixed bottom-1 left-[6%] md:left-[14%] lg:left-[22%] xl:left-[30%] 2xl:left-[34%] shadow-xl"
+                    >
+                        <div className="text-[16px] text-white text-center">
+                            {cartLength} items added
+                        </div>
+                        <Link to={`/cart`}>
+                            <div className="text-[15px] xl:text-[18px] text-white p-2 cursor-pointer hover:scale-105 hover:font-bold duration-300">View Cart</div></Link>
+                    </div>}
                 </div>}
 
             </div>
